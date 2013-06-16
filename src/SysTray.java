18e44690ebf68b7a123aebcac0dea50538fe2db6
Shorble <jerryt67@gmail.com>
2013-06-15 23:35:47 -0600

    Update SysTray.java
    Added variables set by user in Preferences, such as login credentials and booleans for open and available on startup. Added "support" for 'Schedule Me' and 'Preferences' windows. Added methods allowing those windows to modify variables.

--- src/SysTray.java
+++ src/SysTray.java
@@ -1,6 +1,8 @@
 import java.awt.*;
 import java.awt.event.*;
 import javax.imageio.ImageIO;
+import javax.swing.JFrame;
+
 import java.io.IOException;
 
 /** 
@@ -8,12 +10,19 @@ import java.io.IOException;
  * 5/10/13
  * SysTray.java
  * This class is used to access the system tray.
+ * EXIT PROGRAM IF ERROR?!
  */
 
 public class SysTray implements ActionListener
 {
 	private boolean sysTraySupported;
-	private MenuItem scheduleItem, openItem, prefItem, checkItem;
+	private MenuItem scheduleItem, openItem, prefItem, checkItem, quit;
+	private Image iconImage;
+	private String tutorEmail, tutorPassword;
+	private boolean openOnStartup;
+	private boolean availableOnStartup;	
+	private double timeOnStartup;											// constant, set by user
+	private double timeLeft;												// decrement with a timer
 	
 	public SysTray()
 	{
@@ -23,11 +32,17 @@ public class SysTray implements ActionListener
 		openItem = new MenuItem("Open in Browser");
 		prefItem = new MenuItem("Preferences");
 		checkItem = new MenuItem("Check for Notifications");
+		quit = new MenuItem("Exit");
 		
 		scheduleItem.addActionListener(this);
 		openItem.addActionListener(this);
 		prefItem.addActionListener(this);
 		checkItem.addActionListener(this);
+		quit.addActionListener(this);
+		
+		openOnStartup = availableOnStartup = false;
+		timeOnStartup = 0;
+		timeLeft = 0;
 	}
 	
 	public boolean isSupported()
@@ -50,7 +65,7 @@ public class SysTray implements ActionListener
 		
 		SystemTray tray = SystemTray.getSystemTray();
 		
-		Image iconImage = loadImage("logo.png");
+		iconImage = loadImage("logo.png");
 		//scales image to trayIconWidth and height with correct aspect ratio
 		//uses manual scaling to use better scaling algorithm than TrayIcon.setImageAutoSize()
 		int trayIconWidth = new TrayIcon(iconImage).getSize().width;
@@ -61,6 +76,7 @@ public class SysTray implements ActionListener
 		popup.add(openItem);
 		popup.add(prefItem);
 		popup.add(checkItem);
+		popup.add(quit);
 		trayIcon.setPopupMenu(popup); //opens on right click
 		
 		try
@@ -72,6 +88,49 @@ public class SysTray implements ActionListener
 			System.out.println("TrayIcon could not be added.");
         }
 	}
+	
+	public void actionPerformed(ActionEvent e)
+	{
+		Object src = e.getSource();
+		
+		if(src == scheduleItem)
+			schedule();
+		else if(src == openItem)
+			openBrowser();
+		else if(src == prefItem)
+			openPrefs();
+		else if(src == checkItem)
+			checkForNotifs();
+		else if(src == quit)
+			closeApp();
+	}
+	
+	public void schedule()
+	{
+		Schedule popup = new Schedule();
+		popup.setUp(iconImage, "Schedule Me!");
+		popup.setSysTray(this);
+	}
+	public void openBrowser()
+	{
+		System.out.println("SysTray: Open in Browser clicked");
+	}
+	public void openPrefs()
+	{
+		Preferences popup = new Preferences();
+		popup.setUp(iconImage, "Preferences");
+		popup.setSysTray(this);
+	}
+	public void checkForNotifs()
+	{
+		System.out.println("SysTray: Check for notifications clicked");
+	}
+	
+	public void closeApp()
+	{
+		System.exit(1);
+	}
+	
 	//loads an image with the imageName, returns the image, exits program if there is an error
 	public Image loadImage(String imageName) //parameter - name of image file
 	{
@@ -97,34 +156,54 @@ public class SysTray implements ActionListener
 		return returnPic; //returns image
 	}
 	
-	public void actionPerformed(ActionEvent e)
-	{
-		Object src = e.getSource();
+	public void editPreferences(String email, char [] password, boolean startup, boolean available, String time)
+	{	// called by Preferences.java right before it closes
+		tutorEmail = email;
+		String temp = "";
+		for (int i = 0; i < password.length; i++)		// puts the user password together from the char[] array
+		{
+			temp += password[i];
+		}
+		tutorPassword = temp;
 		
-		if(src == scheduleItem)
-			schedule();
-		else if(src == openItem)
-			openBrowser();
-		else if(src == prefItem)
-			openPrefs();
-		else if(src == checkItem)
-			checkForNotifs();
+		openOnStartup = startup;
+		availableOnStartup = available;
+		
+		if (availableOnStartup)
+		{
+			if (time.equals("1 Hour"))
+				timeOnStartup = 1.0;
+			else if (time.equals("1.5 Hours"))
+				timeOnStartup = 1.5;
+			else if (time.equals("2 Hours"))
+				timeOnStartup = 2.0;
+			else if (time.equals("2.5 Hours"))
+				timeOnStartup = 2.5;
+			else if (time.equals("3 Hours"))
+				timeOnStartup = 3.0;
+			else if (time.equals("3.5 Hours"))
+				timeOnStartup = 3.5;
+			else if (time.equals("4 Hours"))
+				timeOnStartup = 4.0;
+		}
 	}
 	
-	public void schedule()
-	{
-		System.out.println("SysTray: Schedule me clicked");
-	}
-	public void openBrowser()
-	{
-		System.out.println("SysTray: Open in Browser clicked");
-	}
-	public void openPrefs()
-	{
-		System.out.println("SysTray: Preferences clicked");
-	}
-	public void checkForNotifs()
-	{
-		System.out.println("SysTray: Check for notifications clicked");
+	public void setAvailability(String time)
+	{	// called by Schedule.java right before it closes
+		if (time.equals("1 Hour"))
+			timeLeft = 1.0;
+		else if (time.equals("1.5 Hours"))
+			timeLeft = 1.5;
+		else if (time.equals("2 Hours"))
+			timeLeft = 2.0;
+		else if (time.equals("2.5 Hours"))
+			timeLeft = 2.5;
+		else if (time.equals("3 Hours"))
+			timeLeft = 3.0;
+		else if (time.equals("3.5 Hours"))
+			timeLeft = 3.5;
+		else if (time.equals("4 Hours"))
+			timeLeft = 4.0;
+		System.out.print(timeLeft);
 	}
 }
