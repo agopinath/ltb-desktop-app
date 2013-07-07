bb293d2ac6d547b5da4fb05faeb83a4568f79160
GouthamR <gouthamrajeev1+github@gmail.com>
2013-07-06 23:09:39 -0700

    created PreferenceDataTester

---
+++ src/PreferenceDataTester.java
@@ -0,0 +1,18 @@
+/**
+ * Tests PreferenceData class.
+ * @author Goutham Rajeev
+ */
+public class PreferenceDataTester
+{
+	public static void main(String[] args)
+	{
+		PreferenceData data = new PreferenceData();
+		data.setPreferences("bob@gmail.com", "pass", true, false, 10);
+		System.out.println("data before save:\n" + data);
+		data.saveToFile();
+		data.setPreferences("bob22@gmail.com", "passWORD", false, true, 20);
+		System.out.println("data modified after save:\n" + data);
+		data.loadFromFile();
+		System.out.println("data after load:\n" + data);
+	}
+}
