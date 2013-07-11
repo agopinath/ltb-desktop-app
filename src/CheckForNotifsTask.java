353b399d06da854abf1a44f56108a5bcf48d8b46
Ajay Gopinath <ajay.gopinath@outlook.com>
2013-07-11 16:14:19 -0700

    making interval between checks for notifications to 5 minutes

--- src/CheckForNotifsTask.java
+++ src/CheckForNotifsTask.java
@@ -1,7 +1,7 @@
 
 public class CheckForNotifsTask implements Runnable 
 {
-	private final static long PING_INTERVAL =  1000 * 60; // for testing purposes, we'll ping every 60,000 ms (i.e. 1 minute)
+	private final static long PING_INTERVAL =  1000 * 300; // for testing purposes, we'll ping every 300 s (i.e. 5 minutes)
 	private MainCoordinator master;
 	
 	public CheckForNotifsTask(MainCoordinator coordinator) 
