cb47ecef63a97c2c64c027ce164901edb53b1f59
GouthamR <gouthamrajeev1+github@gmail.com>
2013-07-15 22:52:49 -0700

    Added NotificationWindowTester class

---
+++ src/NotificationWindowTester.java
@@ -0,0 +1,16 @@
+import java.awt.GraphicsDevice;
+import java.awt.GraphicsDevice.WindowTranslucency;
+import java.awt.GraphicsEnvironment;
+
+import javax.swing.JFrame;
+import javax.swing.SwingUtilities;
+
+
+public class NotificationWindowTester
+{
+	public static void main(String[] args)
+	{
+		new NotificationWindow().showNotification();
+	}
+
+}
