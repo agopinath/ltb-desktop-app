28377c702b06bd0dcbba4cc371d6054cd94ddf49
Ajay Gopinath <ajay.gopinath@outlook.com>
2013-07-27 11:11:38 -0700

    lowering ping interval in CheckForNotifsTask since duration tutor is scheduled to take a request is 5 mins max

--- src/CheckForNotifsTask.java
+++ src/CheckForNotifsTask.java
@@ -1,7 +1,7 @@
 
 public class CheckForNotifsTask implements Runnable 
 {
-	private final static long PING_INTERVAL =  1000 * 300; // for testing purposes, we'll ping every 300 s (i.e. 5 minutes)
+	private final static long PING_INTERVAL =  1000 * 60; // for testing purposes, we'll ping every 300 s (i.e. 5 minutes)
 	private MainCoordinator master;
 	
 	public CheckForNotifsTask(MainCoordinator coordinator) 
