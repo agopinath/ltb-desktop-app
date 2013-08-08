af379474c91b8f815b9e6f3ee1ebd9a04fdb45c0
Ajay Gopinath <ajay.gopinath@outlook.com>
2013-08-07 20:57:47 -0700

    improving ping thread system by introducing random factor into ping intervals to avoid deadlocks

--- src/com/mvjava/net/CheckForNotifsTask.java
+++ src/com/mvjava/net/CheckForNotifsTask.java
@@ -1,11 +1,13 @@
 package com.mvjava.net;
+import java.util.Random;
+
 import com.mvjava.core.MainCoordinator;
 
 
 
 public class CheckForNotifsTask implements Runnable 
 {
-	private final static long PING_INTERVAL =  1000 * 60; // for testing purposes, we'll ping every 300 s (i.e. 5 minutes)
+	private final static long PING_INTERVAL = 1000 * 90; // baseline ping interval is 1.5 mins
 	private MainCoordinator master;
 	
 	public CheckForNotifsTask(MainCoordinator coordinator) 
@@ -23,7 +25,14 @@ public class CheckForNotifsTask implements Runnable
 			
 			try 
 			{
-				Thread.sleep(PING_INTERVAL);
+				// sleep for the baseline ping interval plus an additional
+				// random # of secs (between 0 and 60 secs). This should
+				// (in principle, at least) prevent deadlocks with the 
+				// HTTP server in the event that two tutors' pings coincide
+				long randomFactor = (long)(new Random().nextInt(10))*6000;
+				System.out.println("Sleeping for " + (PING_INTERVAL + randomFactor)/1000 
+									+ " secs.");
+				Thread.sleep(PING_INTERVAL + randomFactor);
 			} catch (InterruptedException e) {
 				e.printStackTrace();
 			}
