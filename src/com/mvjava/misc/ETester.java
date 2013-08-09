eb9912a552289679905ac5f63daea657e1dbe6de
Ajay Gopinath <ajay.gopinath@outlook.com>
2013-08-09 14:28:19 -0700

    deleted old assets folder

---
+++ src/com/mvjava/misc/ETester.java
@@ -0,0 +1,15 @@
+package com.mvjava.misc;
+
+public class ETester {
+	
+	public static void main(String[] args) throws Exception {
+		String a = "hello";
+		String ae = EncryptionHandler.encrypt(a, ETester.class);
+		
+		String b = EncryptionHandler.decrypt(ae, ETester.class);
+		System.out.println("orig: " + a);
+		System.out.println("en: " + ae);
+		System.out.println("un: " + b);
+	}
+
+}
