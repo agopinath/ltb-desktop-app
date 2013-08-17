5bcf36c0088d0cbd486a970c6dbafdb4d308b4fd
GouthamR <gouthamrajeev1+github@gmail.com>
2013-08-16 18:20:28 -0700

    Renamed ETester to EncryptionTester

--- src/com/mvjava/test/EncryptionTester.java
+++ src/com/mvjava/test/EncryptionTester.java
@@ -2,13 +2,13 @@ package com.mvjava.test;
 
 import com.mvjava.misc.EncryptionHandler;
 
-public class ETester {
+public class EncryptionTester {
 	
 	public static void main(String[] args) throws Exception {
 		String a = "hello";
-		String ae = EncryptionHandler.encrypt(a, ETester.class);
+		String ae = EncryptionHandler.encrypt(a, EncryptionTester.class);
 		
-		String b = EncryptionHandler.decrypt(ae, ETester.class);
+		String b = EncryptionHandler.decrypt(ae, EncryptionTester.class);
 		System.out.println("orig: " + a);
 		System.out.println("en: " + ae);
 		System.out.println("un: " + b);
